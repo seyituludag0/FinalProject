558eda8df394e88fcf51b050be0cc4e8c949028a
Seyit Uludağ <seyituludag0@outlook.com>
2021-01-31 00:41:38 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+    }
+}
