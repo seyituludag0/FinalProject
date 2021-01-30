5e808ca866263a6ce884b7811061662a4bd01ef7
Seyit Uludağ <seyituludag0@outlook.com>
2021-01-31 01:50:04 +0300

    FinalProject

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
             ProductManager productManager = new ProductManager(new InMemoryProductDal());
             foreach (var product in productManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine("Ürünün Adı: " + product.ProductName + "\n" + "Ürünün Fiyatı: " + product.UnitPrice + "\n");
             }
             Console.ReadLine();
         }
