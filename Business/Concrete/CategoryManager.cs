b5d5ed63dd82db58771ee9346681bafa1eb8a035
Seyit Uludağ <seyituludag0@outlook.com>
2021-02-06 18:57:20 +0300

    .

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager:ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,19 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAllByCategoryId(5))
+            //ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            //foreach (var product in productManager.GetAllByCategoryId(5))
+            //{
+            //    Console.WriteLine("Ürünün Adı: " + product.ProductName + "\n" + "Ürünün Fiyatı: " + product.UnitPrice + "\n");
+            //}
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine("Ürünün Adı: " + product.ProductName + "\n" + "Ürünün Fiyatı: " + product.UnitPrice + "\n");
+                Console.WriteLine("Category Id: " + category.CategoryId + " Category Name: " + category.CategoryName);
             }
+
             Console.ReadLine();
         }
     }
