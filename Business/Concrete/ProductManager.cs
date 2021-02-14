5ed01d35b0cadae818103090f1e144356fceb4e7
Seyit Uludağ <seyituludag0@outlook.com>
2021-02-14 18:41:39 +0300

    13 Şubat

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -40,7 +40,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 19)
+            if (DateTime.Now.Hour == 23 )
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
