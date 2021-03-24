b2cab0ce077e9f646dc14e2a0354d291485a191f
seyituludag0 <seyituludag0@gmail.com>
2021-03-24 20:54:08 +0300

    24 Mart

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -30,7 +30,7 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
-            Thread.Sleep(1000);
+            //Thread.Sleep(1000);
 
             var result = _productService.GetAll();
             if (result.Success)
